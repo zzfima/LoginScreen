ba0aad3c5493cbffbd33cf1a420ffdc83237e3d2
zzfima <zzfima@gmail.com>
2025-04-04 17:20:46 +0300

    Add project files.

---
+++ LoginScreen/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.SQLite;
+using System.Windows;
+
+namespace LoginScreen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            lblSuccess.Text = string.Empty;
+            lblError.Text = string.Empty;
+
+            var connectionString = "Data Source=..\\..\\..\\DB\\LoginDB.db;Version=3;";
+            var readCommand = $"SELECT * FROM Users WHERE Username = '{txtUsername.Text}' AND Password = '{txtPassword.Password}'";
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                using (var cmd = new SQLiteCommand(readCommand, connection))
+                using (var reader = cmd.ExecuteReader())
+                {
+                    bool isExist = reader.HasRows;
+                    if (isExist)
+                    {
+                        lblSuccess.Text = "Login successful!";
+                    }
+                    else
+                    {
+                        lblError.Text = "Invalid username or password.";
+                    }
+                }
+            }
+        }
+    }
+}
