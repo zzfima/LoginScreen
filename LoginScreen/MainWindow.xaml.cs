a763c1123b824a1df7ad0d3ca4c481a1e8ce274e
zzfima <zzfima@gmail.com>
2025-04-04 17:48:00 +0300

    add hashed password

--- LoginScreen/MainWindow.xaml.cs
+++ LoginScreen/MainWindow.xaml.cs
@@ -18,25 +18,84 @@ namespace LoginScreen
             lblSuccess.Text = string.Empty;
             lblError.Text = string.Empty;
 
+            var isExist = ValidateUserSafe(txtUsername.Text, txtPassword.Password);
+            if (isExist)
+            {
+                lblSuccess.Text = "Login successful!";
+            }
+            else
+            {
+                lblError.Text = "Invalid username or password.";
+            }
+        }
+
+
+        private void AddUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            RegisterUserSafe(txtUsername.Text, txtPassword.Password);
+        }
+
+        //bad practice
+        /*
+            1. SQL Injection: The query is vulnerable to SQL injection because user input is directly concatenated into the SQL query.
+            2. Plaintext Passwords: Storing passwords as plaintext is a huge security risk.
+         */
+        public bool ValidateUser(string username, string password)
+        {
             var connectionString = "Data Source=..\\..\\..\\DB\\LoginDB.db;Version=3;";
-            var readCommand = $"SELECT * FROM Users WHERE Username = '{txtUsername.Text}' AND Password = '{txtPassword.Password}'";
+            var readCommand = $"SELECT * FROM Users WHERE Username = '{username}' AND Password = '{password}'";
+
             using (var connection = new SQLiteConnection(connectionString))
             {
                 connection.Open();
-                using (var cmd = new SQLiteCommand(readCommand, connection))
-                using (var reader = cmd.ExecuteReader())
+                var command = new SQLiteCommand(readCommand, connection);
+                using (var reader = command.ExecuteReader())
                 {
-                    bool isExist = reader.HasRows;
-                    if (isExist)
-                    {
-                        lblSuccess.Text = "Login successful!";
-                    }
-                    else
-                    {
-                        lblError.Text = "Invalid username or password.";
-                    }
+                    return reader.HasRows;
                 }
             }
         }
+
+        //good practice
+        /*
+            1. Use Parameterized Queries: This prevents SQL injection attacks.
+            2. Hash Passwords: Store hashed passwords instead of plaintext.
+         */
+        public bool ValidateUserSafe(string username, string password)
+        {
+            var connectionString = "Data Source=..\\..\\..\\DB\\LoginDB.db;Version=3;";
+            var readCommand = "SELECT password FROM Users WHERE Username = @username";
+
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                using (var command = new SQLiteCommand(readCommand, connection))
+                {
+                    command.Parameters.AddWithValue("@username", username);
+                    var hash = command.ExecuteScalar() as string;
+                    if (hash == null)
+                        return false;
+
+                    return BCrypt.Net.BCrypt.Verify(password, hash);
+                }
+            }
+        }
+
+        public void RegisterUserSafe(string username, string password)
+        {
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+            var connectionString = "Data Source=..\\..\\..\\DB\\LoginDB.db;Version=3;";
+            var insertCommand = "INSERT INTO Users (username, password) VALUES (@username, @password)";
+
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                var command = new SQLiteCommand(insertCommand, connection);
+                command.Parameters.AddWithValue("@username", username);
+                command.Parameters.AddWithValue("@password", hashedPassword);
+                command.ExecuteNonQuery();
+            }
+        }
+
     }
 }
